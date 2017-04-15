6223440586b974768f864d24d1df53f3848319a3
NechepurencoIvan <ivan.nechepurenco@yandex.ru>
2017-04-15 20:27:03 +0300

    Add files via upload

---
+++ creation.sql
@@ -0,0 +1,11 @@
+﻿--DROP TABLE donors;
+--DROP TABLE ill;
+--DROP TABLE donors_ill;
+--DROP TABLE persons;
+CREATE TABLE persons (person_id int, fam text, name text, otch text, contact_id int);
+CREATE TABLE donors (donor_id int,person_id int, bllood_gr int,
+rf boolean,born date, sex text,work text, passed int, lastpass date);
+CREATE TABLE ill (ill_id int, person_id int, bllood_gr int,
+rf boolean, disease text,volume int);
+CREATE TABLE donors_ill (donor_id int, ill_id int, confirm bool);
+--SELECT * FROM donors, ill, donors_ill;
